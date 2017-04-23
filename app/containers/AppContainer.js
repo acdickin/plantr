07b2cc7e57b3303c1c3a19c61b1e71789dc0417f
Andrew Dickinson <andrew.dickinson@t2.local>
2017-04-23 12:43:10 -0700

    Add one action

--- app/containers/AppContainer.js
+++ app/containers/AppContainer.js
@@ -1,26 +1,40 @@
 import React, { Component } from 'react';
 import ReactNative from 'react-native';
-
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { ActionCreators } from '../actions';
 
 const {
   View,
   Text,
-
+  TouchableHighlight,
 } =ReactNative
 
 class AppContainer extends Component{
   constructor(props){
     super(props);
+    this.state ={
+      plantNum:0
+    }
+  }
+  incrementPlantCount(){
+    this.setState({  plantNum:this.state.plantNum+1})
   }
   render(){
     return(
       <View>
         <Text style={{marginTop:20}}>Title</Text>
+        <TouchableHighlight onPress={()=>{this.incrementPlantCount() }} >
+          <Text> Add Plant</Text>
+        </TouchableHighlight>
+        <Text> Plant Count {this.state.plantNum}</Text>
       </View>
     )
   }
 }
 
+function mapDispatchToProps(dispatch){
+  return bindActionCreators(ActionCreators,dispatch);
+}
 
-
-export default AppContainer
+export default connect (()=>{return {}},mapDispatchToProps)(AppContainer)
